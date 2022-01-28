b2fe281c5f9f4422d6c4ecd4cb4f1938e2037892
JimmyHoffa <1002984+JimmyHoffa@users.noreply.github.com>
2022-01-28 13:04:45 -0700

    Accidental copy-paste wrong spot

--- src/entities/blockSummary.ts
+++ src/entities/blockSummary.ts
@@ -1,26 +1,14 @@
-export type BlockPowSolutions = {
-  pk: string
-  w: string
-  n: string
-  d: number
-}
-
-export type BlockHeader = {
-  timestamp: bigint
-  height: number
-  nBits: number
-  version: number
-  extensionId: string
-  difficulty: string
-  votes: string
-  size: string
-  stateRoot: string
+export type BlockSummary = {
   id: string
-  adProofsRoot: string
-  transactionsRoot: string
-  extensionHash: string
-  powSolutions: BlockPowSolutions
-  adProofsId: string
-  transactionsId: string
-  parentId: string
+  height: number
+  epoch: number
+  version: number
+  timestamp: bigint
+  transactionsCount: number
+  miner: {
+    address: string
+    name: string
+  }
+  size: number
+  difficulty: bigint
 }
